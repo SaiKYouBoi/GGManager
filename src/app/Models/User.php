a23b3a37404aacb4ab51e30774d9ae0232a6a57a
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-03-24 17:29:43 +0100

    auth with laravel sanctum

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -2,31 +2,50 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens; 
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role', 
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'role' => 'string', 
         ];
+    }
+
+    /**
+     * Relationships based on your ERD
+     */
+
+    // User organizes many Tournaments
+    public function tournaments(): HasMany
+    {
+        return $this->hasMany(Tournament::class, 'organizer_id');
+    }
+
+    // User has many Registrations
+    public function registrations(): HasMany
+    {
+        return $this->hasMany(Registration::class);
     }
 }
