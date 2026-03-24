ee32b1d409905deaa099c192b867d09ac0e74dc5
zakarya hari <zakariahari42@gmail.com>
2026-03-24 16:40:25 +0100

    feat : autorisation de suppression uniquement pour le propriétaire avec statut open

--- src/app/Policies/TournamentPolicy.php
+++ src/app/Policies/TournamentPolicy.php
@@ -29,7 +29,7 @@ class TournamentPolicy
 
     public function delete(User $user, Tournament $tournament): bool
     {
-        return false;
+        return $user->id === $tournament->organizer_id && $tournament->status === 'open';
     }
 
     public function restore(User $user, Tournament $tournament): bool
