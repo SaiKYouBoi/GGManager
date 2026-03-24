bf761befe165dcbfa9a45402bc18c36806ef3e39
zakarya hari <zakariahari42@gmail.com>
2026-03-24 16:40:10 +0100

    feat : autorisation de mise à jour uniquement pour le propriétaire avec statut open

--- src/app/Policies/TournamentPolicy.php
+++ src/app/Policies/TournamentPolicy.php
@@ -24,7 +24,7 @@ class TournamentPolicy
 
     public function update(User $user, Tournament $tournament): bool
     {
-        return false;
+        return $user->id === $tournament->organizer_id && $tournament->status === 'open';
     }
 
     public function delete(User $user, Tournament $tournament): bool
