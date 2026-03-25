ec6396ecda9636c49c842e933275a99b02d97453
zakarya hari <zakariahari42@gmail.com>
2026-03-25 10:01:08 +0100

    feat : ajout des routes protégées store, update et destroy via middleware auth:sanctum

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('tournaments', TournamentController::class)
     ->only(['index', 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('tournaments', TournamentController::class)
+        ->only(['store', 'update', 'destroy']);
+});
