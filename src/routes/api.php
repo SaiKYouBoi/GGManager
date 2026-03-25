cb305e8c2e8ce909b8299665187e1416b33d69bd
SaiKYouBoi <iliaselggames@gmail.com>
2026-03-25 16:58:55 +0100

    fix : auth api routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,6 +1,6 @@
 <?php
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TournamentController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
@@ -16,10 +16,6 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-});
-
-Route::get('/test', function () {
-    return "API working";
 });
 
 Route::apiResource('tournaments', TournamentController::class)
