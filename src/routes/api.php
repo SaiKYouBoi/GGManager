5d62b8c2614fb893d52d2c954196e1f1b7f00082
SaiKYouBoi <iliaselggames@gmail.com>
2026-03-25 16:48:42 +0100

    fix : fix issues in auth

--- src/routes/api.php
+++ src/routes/api.php
@@ -10,9 +10,13 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    
-   
+
+
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+});
+
+Route::get('/test', function () {
+    return "API working";
 });
