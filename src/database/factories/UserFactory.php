57c559306357a0f01fffbde3542660574cde1cea
SaiKYouBoi <iliaselggames@gmail.com>
2026-03-26 15:02:37 +0100

    feat : add role to UserFactory

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -30,6 +30,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role' => 'player',
         ];
     }
 
@@ -41,5 +42,15 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    public function organizer(): static
+    {
+        return $this->state(['role' => 'organizer']);
+    }
+
+    public function player(): static
+    {
+        return $this->state(['role' => 'player']);
     }
 }
